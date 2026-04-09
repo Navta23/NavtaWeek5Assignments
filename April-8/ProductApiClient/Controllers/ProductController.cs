4e59b1c3b6f55414268714e0799de37a27bf9261
Navta23 <guptanavta@gmail.com>
2026-04-09 10:27:09 +0530

    added

---
+++ April-8/ProductApiClient/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductApiClient.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IConfiguration _config;
+        public ProductController(IConfiguration config)
+        {
+            _config = config;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.ApiBaseUrl = _config["ApiSettings:BaseUrl"];
+            return View();
+        }
+    }
+}
